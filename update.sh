07ebe7d841d03f4e06bd34811dd98185b0bb03d6
Nick <nick.young@auckland.ac.nz>
2024-09-25 11:33:11 +1200

    update update.sh to automatically tag and release. add zenodo badge

--- update.sh
+++ update.sh
@@ -4,3 +4,7 @@ git pull
 jupyter nbconvert --to notebook --execute --inplace tidal_correction.ipynb linear_models.ipynb
 git commit -am "auto update" --author="coastsat-bot <ubuntu@wave.storm-surge.cloud.edu.au>"
 git push
+TAG=$(date -u +"%Y-%m-%dT%H-%M-%SZ")
+git tag $TAG
+git push origin $TAG
+gh release create $TAG --generate-notes
