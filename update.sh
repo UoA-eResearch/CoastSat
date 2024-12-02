a5853d004a2b7aa5c2c26776430e82f7a9c66a34
Nick <nick.young@auckland.ac.nz>
2024-12-02 18:48:35 +1300

    git add ., add nzd0563 folder

--- update.sh
+++ update.sh
@@ -2,6 +2,7 @@
 git pull
 ./batch_process.py || (echo "Batch process failed" && exit 1)
 jupyter nbconvert --to notebook --execute --inplace tidal_correction.ipynb linear_models.ipynb
+git add .
 git commit -am "auto update" --author="coastsat-bot <ubuntu@wave.storm-surge.cloud.edu.au>"
 git push
 TAG=$(date -u +"%Y-%m-%dT%H-%M-%SZ")
