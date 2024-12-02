23818d504dbf4ed530e1df9e55615dc1241785c7
Nick <nick.young@auckland.ac.nz>
2024-12-03 11:50:52 +1300

    automate slope estimation for new transects

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git pull
 ./batch_process.py || (echo "Batch process failed" && exit 1)
-jupyter nbconvert --to notebook --execute --inplace tidal_correction.ipynb linear_models.ipynb
+jupyter nbconvert --to notebook --execute --inplace tidal_correction.ipynb slope_estimation.ipynb linear_models.ipynb
 git add .
 git commit -am "auto update" --author="coastsat-bot <ubuntu@wave.storm-surge.cloud.edu.au>"
 git push
