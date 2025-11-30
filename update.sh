4387f015cd4d0d389450caab5387c186a6d5d2ed
Nick Young <nick.young@auckland.ac.nz>
2025-12-01 08:59:05 +1300

    move second execution to a new line

--- update.sh
+++ update.sh
@@ -4,7 +4,8 @@ git pull
 ./batch_process_sar.py
 # For new sites, first we need to run tidal_correction to fetch the tides, then we can run slope_estimation, then we can use the slopes to apply the tidal correction
 # This is why tidal_correction.ipynb is run twice
-jupyter nbconvert --to notebook --execute --inplace tidal_correction.ipynb slope_estimation.ipynb tidal_correction.ipynb linear_models.ipynb
+jupyter nbconvert --to notebook --execute --inplace tidal_correction.ipynb slope_estimation.ipynb
+jupyter nbconvert --to notebook --execute --inplace tidal_correction.ipynb linear_models.ipynb
 ./make_xlsx.py
 git add .
 git commit -am "auto update" --author="coastsat-bot <ubuntu@wave.storm-surge.cloud.edu.au>"
